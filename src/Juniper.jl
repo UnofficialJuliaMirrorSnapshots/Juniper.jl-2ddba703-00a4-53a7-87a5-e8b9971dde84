0e79a4b14a299e43aa21a6230cbf322874b88186
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 23:09:08 +0000

    Snapshot of branch master taken on 2019-04-07T19:09:07.842-04:00 from "https://github.com/UnofficialJuliaMirror/Juniper.jl-2ddba703-00a4-53a7-87a5-e8b9971dde84"

---
+++ src/Juniper.jl
@@ -0,0 +1,51 @@
+module Juniper
+
+using MathProgBase
+using JuMP
+using JSON
+
+if VERSION < v"0.7.0-"
+    import Compat: occursin
+    import Compat: Nothing
+    import Compat: round
+    import Compat: @warn
+    import Compat: pushfirst!
+    import Compat: popfirst!
+    import Compat: Array
+    import Compat: undef
+    import Compat: hasmethod
+    import Compat: findall
+end
+
+if VERSION > v"0.7.0-"
+    using LinearAlgebra
+    using Random
+    using Distributed
+    using Statistics
+end
+
+
+
+include("types.jl")
+
+function Base.show(io::IO, opts::SolverOptions)
+    longest_field_name = maximum([length(string(fname)) for fname in fieldnames(SolverOptions)])+2
+    for name in fieldnames(SolverOptions)
+        sname = string(name)
+        pname = sname*repeat(" ", longest_field_name-length(sname))
+        if getfield(opts,name) == nothing
+            println(io, pname, ": NA")
+        else
+            println(io, pname, ": ", getfield(opts,name))
+        end
+    end
+end
+
+include("util.jl")
+include("printing.jl")
+include("solver.jl")
+include("model.jl")
+include("BnBTree.jl")
+
+
+end
